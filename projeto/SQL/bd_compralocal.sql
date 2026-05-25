85743fe7d0c1c1a109fb3735af66d0152c833e91
Matheus Washington <matheusalmeida1809@outlook.com>
2026-05-25 11:52:01 -0300

    Finaly database and add table estado

--- projeto/SQL/bd_compralocal.sql
+++ projeto/SQL/bd_compralocal.sql
@@ -8,24 +8,29 @@ CREATE TABLE cliente (
 	nome  VARCHAR (100) NOT NULL,
 	senha VARCHAR (150) NOT NULL
 );
+CREATE TABLE estado (
+	id_estado INT PRIMARY KEY AUTO_INCREMENT,
+    nome_estado ENUM ('AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 
+    'MS', 'MT', 'PA', 'PI', 'PB', 'PE', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO'),
+    regiao ENUM ('CENTRO-OESTE', 'NORTE', 'NORDESTE', 'SUL', 'SUDESTE')
+    );
 CREATE TABLE endereco_cliente (
 	id_endereco INT PRIMARY KEY AUTO_INCREMENT,
-	estado  CHAR (2) NOT NULL CHECK (estado REGEXP '^[A-Z]{2}$'),
+	id_estado INT NOT NULL,
 	cidade VARCHAR (100) NOT NULL,
 	rua VARCHAR (150) NOT NULL,
 	numero VARCHAR (8) NOT NULL  CHECK (numero REGEXP '^[0-9]$'),
 	cep CHAR (8) NOT NULL CHECK (cep REGEXP '^[0-9]{8}$'),
-	num_telefone CHAR (11) NOT NULL  CHECK (num_telefone REGEXP '^[0-9]$')
+	num_telefone CHAR (11) NOT NULL  CHECK (num_telefone REGEXP '^[0-9]$'),
+    FOREIGN KEY (id_estado)
+    REFERENCES estado (id_estado)
 );
-CREATE TABLE reside (
-	id_endereco INT,
+CREATE TABLE pedido (
+	id_pedido INT PRIMARY KEY AUTO_INCREMENT,
 	id_cliente INT,
-	FOREIGN KEY (id_endereco) REFERENCES endereco_cliente (id_endereco),
-	FOREIGN KEY (id_cliente) REFERENCES cliente (id_cliente),
-	PRIMARY KEY (id_endereco, id_cliente)
-);
-CREATE TABLE carrinho (
-	id_carrinho INT PRIMARY KEY AUTO_INCREMENT
+    data_pedido DATETIME,
+	FOREIGN KEY (id_cliente) 
+    REFERENCES cliente (id_cliente)
 );
 CREATE TABLE cupom (
 	id_cupom INT PRIMARY KEY AUTO_INCREMENT,
@@ -33,14 +38,14 @@ CREATE TABLE cupom (
 	taxa_desconto DECIMAL NOT NULL,
 	descricao  VARCHAR (100) NOT NULL,
 	origem ENUM ('loja', 'plataforma') DEFAULT "plataforma" NOT NULL,
-	STATUS ENUM ('ativo', 'inativo') DEFAULT "ativo" NOT NULL
+	status_cupom ENUM ('ativo', 'inativo') DEFAULT "ativo" NOT NULL
 );
 CREATE TABLE aplica (
 	id_cupom INT,
-	id_carrinho INT,
+    id_pedido INT,
 	FOREIGN KEY (id_cupom) REFERENCES cupom (id_cupom),
-	FOREIGN KEY (id_carrinho) REFERENCES carrinho (id_carrinho),
-	PRIMARY KEY (id_cupom, id_carrinho)
+	FOREIGN KEY (id_pedido) REFERENCES pedido (id_pedido),
+	PRIMARY KEY (id_cupom, id_pedido)
 );
 CREATE TABLE lojista (
 	id_loja INT PRIMARY KEY AUTO_INCREMENT,
@@ -49,11 +54,13 @@ CREATE TABLE lojista (
 	cnpj CHAR (14) NOT NULL UNIQUE CHECK (cnpj REGEXP '^[0-9]{14}$'),
 	nome_loja VARCHAR (100) NOT NULL,
 	cep CHAR (8) NOT NULL CHECK (cep REGEXP '^[0-9]{8}$'),
-	estado CHAR (2) NOT NULL CHECK (estado REGEXP '^[A-Z]{2}$'),
+	id_estado INT,
 	cidade VARCHAR (100) NOT NULL,
 	rua VARCHAR (150) NOT NULL,
 	numero VARCHAR (8) NOT NULL CHECK (numero REGEXP '^[0-9]$'),
-	UNIQUE (cep, estado, cidade, rua, numero)
+	UNIQUE (cep, cidade, rua, numero),
+	FOREIGN KEY (id_estado)
+    REFERENCES estado (id_estado)
 );
 CREATE TABLE entregador (
 	id_entregador INT PRIMARY KEY AUTO_INCREMENT,
@@ -61,24 +68,29 @@ CREATE TABLE entregador (
 	nome VARCHAR (100) NOT NULL,
 	senha VARCHAR (150) NOT NULL,
 	email VARCHAR (100) NOT NULL UNIQUE,
-	estado CHAR (2) NOT NULL CHECK (estado REGEXP '^[A-Z]{2}+$'),
+	id_estado INT,
 	cidade VARCHAR (100) NOT NULL,
 	rua VARCHAR (100) NOT NULL,
 	numero VARCHAR (10) NOT NULL CHECK (numero REGEXP '^[0-9A-Za-z]+$'),
-	cep CHAR (8) NOT NULL CHECK (cep REGEXP '^[0-9]{8}$')
+	cep CHAR (8) NOT NULL CHECK (cep REGEXP '^[0-9]{8}$'),
+	FOREIGN KEY (id_estado)
+    REFERENCES estado (id_estado)
 );
 CREATE TABLE entrega (
 	id_entrega INT PRIMARY KEY AUTO_INCREMENT,
 	data_prevista DATE NOT NULL,
 	data_efetiva DATE,
 	status_entrega ENUM ('pendente', 'em transito', 'entregue', 'cancelada') DEFAULT "em transito" NOT NULL,
-	estado CHAR (2) NOT NULL CHECK (estado REGEXP '^[A-Z]$'),
+	id_estado INT,
 	cidade VARCHAR (100) NOT NULL,
 	rua VARCHAR (150) NOT NULL,
 	numero VARCHAR (8) NOT NULL,
 	cep CHAR (8) NOT NULL CHECK (cep REGEXP '^[0-9]{8}$'),
 	id_entregador INT NOT NULL,
-	FOREIGN KEY (id_entregador) REFERENCES entregador (id_entregador)
+	FOREIGN KEY (id_entregador) 
+    REFERENCES entregador (id_entregador),
+	FOREIGN KEY (id_estado)
+    REFERENCES estado (id_estado)
 );
 CREATE TABLE motivo (
 	id_motivo INT PRIMARY KEY AUTO_INCREMENT,
@@ -140,8 +152,15 @@ CREATE TABLE produto (
 	id_categoria INT NOT NULL,
 	id_promocao INT,
 	FOREIGN KEY (id_loja) REFERENCES lojista (id_loja),
-	FOREIGN KEY (id_categoria) REFERENCES categoria (id_categoria),
-	FOREIGN KEY (id_promocao) REFERENCES promocao (id_promocao)
+	FOREIGN KEY (id_categoria) REFERENCES categoria (id_categoria)
+);
+CREATE TABLE promo_prod (
+	id_promocao INT,
+	id_produto INT,
+	FOREIGN KEY (id_promocao)
+	REFERENCES promocao (id_promocao),
+	FOREIGN KEY (id_produto) 
+	REFERENCES produto (id_produto)
 );
 CREATE TABLE avaliacao (
 	id_avaliacao INT PRIMARY KEY AUTO_INCREMENT,
@@ -149,13 +168,10 @@ CREATE TABLE avaliacao (
 	data_avaliacao DATE NOT NULL,
 	id_cliente INT NOT NULL,
 	id_produto INT NOT NULL,
-	FOREIGN KEY (id_cliente) REFERENCES cliente (id_cliente),
-	FOREIGN KEY (id_produto) REFERENCES produto (id_produto)
-);
-CREATE TABLE pedido (
-	id_pedido INT PRIMARY KEY AUTO_INCREMENT,
-	id_carrinho INT NOT NULL,
-	FOREIGN KEY (id_carrinho) REFERENCES carrinho (id_carrinho)
+	FOREIGN KEY (id_cliente) 
+    REFERENCES cliente (id_cliente),
+	FOREIGN KEY (id_produto) 
+    REFERENCES produto (id_produto)
 );
 CREATE TABLE itens_pedido (
 	id_item_pedido INT PRIMARY KEY AUTO_INCREMENT,
@@ -163,8 +179,10 @@ CREATE TABLE itens_pedido (
 	preco_uni DECIMAL NOT NULL,
 	id_pedido INT NOT NULL,
 	id_produto INT NOT NULL,
-	FOREIGN KEY (id_pedido) REFERENCES pedido (id_pedido),
-	FOREIGN KEY (id_produto) REFERENCES produto (id_produto)
+	FOREIGN KEY (id_pedido) 
+    REFERENCES pedido (id_pedido),
+	FOREIGN KEY (id_produto) 
+    REFERENCES produto (id_produto)
 );
 CREATE TABLE pagamento (
 	id_pagamento INT PRIMARY KEY AUTO_INCREMENT,
