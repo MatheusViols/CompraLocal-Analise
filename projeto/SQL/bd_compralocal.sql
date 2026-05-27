9b8916c0f3e9750720f25a52fcafdcdd7ab42590
MatheusViols <matheusviolss@gmail.com>
2026-05-27 14:32:31 -0300

    fix: Correção de DECIMAL para DECIMAL(10, 2)

--- projeto/SQL/bd_compralocal.sql
+++ projeto/SQL/bd_compralocal.sql
@@ -11,22 +11,22 @@ CREATE TABLE cliente (
 
 CREATE TABLE estado (
 	id_estado INT PRIMARY KEY AUTO_INCREMENT,
-    nome_estado ENUM (
+	nome_estado ENUM (
 		'AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG',
 		'MS', 'MT', 'PA', 'PI', 'PB', 'PE', 'PR', 'RJ', 'RN', 'RO', 'RR',
 		'RS', 'SC', 'SE', 'SP', 'TO') NOT NULL,
-    regiao ENUM (
+	regiao ENUM (
 		'CENTRO-OESTE'
 		,'NORTE',
 		'NORDESTE',
-        'SUL',
-        'SUDESTE') NOT NULL
+		'SUL',
+		'SUDESTE') NOT NULL
 );
 
 CREATE TABLE endereco_cliente (
 	id_endereco INT PRIMARY KEY AUTO_INCREMENT,
 	id_estado INT NOT NULL,
-    id_cliente INT NOT NULL,
+	id_cliente INT NOT NULL,
 	cidade VARCHAR (100) NOT NULL,
 	rua VARCHAR (150) NOT NULL,
 	numero VARCHAR (8) NOT NULL CHECK (numero REGEXP '^[0-9]+$'),
@@ -34,10 +34,10 @@ CREATE TABLE endereco_cliente (
 	num_telefone CHAR (11) NOT NULL CHECK (num_telefone REGEXP '^[0-9]{11}$'),
 
 	FOREIGN KEY (id_cliente)
-    REFERENCES cliente (id_cliente),
+	REFERENCES cliente (id_cliente),
 
 	FOREIGN KEY (id_estado)
-    REFERENCES estado (id_estado)
+	REFERENCES estado (id_estado)
 );
 
 CREATE TABLE lojista (
@@ -54,7 +54,7 @@ CREATE TABLE lojista (
 	UNIQUE (cep, cidade, rua, numero),
 
 	FOREIGN KEY (id_estado)
-    REFERENCES estado (id_estado)
+	REFERENCES estado (id_estado)
 );
 
 CREATE TABLE entregador (
@@ -70,7 +70,7 @@ CREATE TABLE entregador (
 	cep CHAR (8) NOT NULL CHECK (cep REGEXP '^[0-9]{8}$'),
 
 	FOREIGN KEY (id_estado)
-    REFERENCES estado (id_estado)
+	REFERENCES estado (id_estado)
 );
 
 CREATE TABLE campanha_vigente (
@@ -83,7 +83,7 @@ CREATE TABLE campanha_vigente (
 
 CREATE TABLE taxa_comissao (
 	id_taxa INT PRIMARY KEY AUTO_INCREMENT,
-	taxa DECIMAL NOT NULL,
+	taxa DECIMAL(10, 2) NOT NULL,
 	id_campanha INT NOT NULL,
 
 	FOREIGN KEY (id_campanha)
@@ -101,7 +101,7 @@ CREATE TABLE categoria (
 
 CREATE TABLE produto (
 	id_produto INT PRIMARY KEY AUTO_INCREMENT,
-	preco DECIMAL NOT NULL,
+	preco DECIMAL(10, 2) NOT NULL,
 	qtd_estoque INT NOT NULL,
 	nome VARCHAR (100) NOT NULL,
 	id_loja INT NOT NULL,
@@ -118,8 +118,8 @@ CREATE TABLE promocao (
 	id_promocao INT PRIMARY KEY AUTO_INCREMENT,
 	data_inicio DATETIME NOT NULL,
 	data_fim DATETIME NOT NULL,
-	preco_anterior DECIMAL NOT NULL,
-	preco_promocional DECIMAL NOT NULL
+	preco_anterior DECIMAL(10, 2) NOT NULL,
+	preco_promocional DECIMAL(10, 2) NOT NULL
 );
 
 CREATE TABLE promo_prod (
@@ -131,42 +131,42 @@ CREATE TABLE promo_prod (
 
 	FOREIGN KEY (id_produto)
 	REFERENCES produto (id_produto),
-    
-    PRIMARY KEY (id_promocao, id_produto)
+
+	PRIMARY KEY (id_promocao, id_produto)
 );
 
 CREATE TABLE pedido (
 	id_pedido INT PRIMARY KEY AUTO_INCREMENT,
 	id_cliente INT NOT NULL,
 	data_pedido DATETIME NOT NULL,
-    status_pedido ENUM (
+	status_pedido ENUM (
 		'aberto',
-        'fechado',
-        'cancelado') 
-        DEFAULT 'aberto' NOT NULL,
+		'fechado',
+		'cancelado') 
+	DEFAULT 'aberto' NOT NULL,
 
 	FOREIGN KEY (id_cliente)
-    REFERENCES cliente (id_cliente)
+	REFERENCES cliente (id_cliente)
 );
 
 CREATE TABLE itens_pedido (
 	id_item_pedido INT PRIMARY KEY AUTO_INCREMENT,
 	qtd_item INT NOT NULL,
-	preco_uni DECIMAL NOT NULL,
+	preco_uni DECIMAL(10, 2) NOT NULL,
 	id_pedido INT NOT NULL,
 	id_produto INT NOT NULL,
 
 	FOREIGN KEY (id_pedido)
-    REFERENCES pedido (id_pedido),
+	REFERENCES pedido (id_pedido),
 
 	FOREIGN KEY (id_produto)
-    REFERENCES produto (id_produto)
+	REFERENCES produto (id_produto)
 );
 
 CREATE TABLE cupom (
 	id_cupom INT PRIMARY KEY AUTO_INCREMENT,
 	cod_promocional VARCHAR (20) NOT NULL,
-	taxa_desconto DECIMAL NOT NULL,
+	taxa_desconto DECIMAL(10, 2) NOT NULL,
 	descricao VARCHAR (100) NOT NULL,
 	origem ENUM ('loja', 'plataforma') DEFAULT 'plataforma' NOT NULL,
 	status_cupom ENUM ('ativo', 'inativo') DEFAULT 'ativo' NOT NULL
@@ -174,8 +174,8 @@ CREATE TABLE cupom (
 
 CREATE TABLE aplica (
 	id_cupom INT NOT NULL,
-    id_pedido INT NOT NULL,
-    id_cliente INT NOT NULL,
+	id_pedido INT NOT NULL,
+	id_cliente INT NOT NULL,
 
 	FOREIGN KEY (id_cupom)
 	REFERENCES cupom (id_cupom),
@@ -184,16 +184,16 @@ CREATE TABLE aplica (
 	REFERENCES pedido (id_pedido),
 
 	FOREIGN KEY (id_cliente)
-    REFERENCES cliente (id_cliente),
-    
+	REFERENCES cliente (id_cliente),
+
 	PRIMARY KEY (id_cupom, id_pedido),
-    
-    UNIQUE (id_cliente, id_cupom)
+
+	UNIQUE (id_cliente, id_cupom)
 );
 
 CREATE TABLE pagamento (
 	id_pagamento INT PRIMARY KEY AUTO_INCREMENT,
-	valor_pago DECIMAL NOT NULL,
+	valor_pago DECIMAL(10, 2) NOT NULL,
 	data_pagamento DATETIME NOT NULL,
 	id_cliente INT NOT NULL,
 	id_pedido INT NOT NULL,
@@ -208,9 +208,9 @@ CREATE TABLE pagamento (
 CREATE TABLE nota_fiscal (
 	id_nota_fiscal INT PRIMARY KEY AUTO_INCREMENT,
 	id_pagamento INT NOT NULL,
-    
-    FOREIGN KEY (id_pagamento)
-    REFERENCES pagamento (id_pagamento)
+
+	FOREIGN KEY (id_pagamento)
+	REFERENCES pagamento (id_pagamento)
 );
 
 CREATE TABLE entrega (
@@ -230,15 +230,15 @@ CREATE TABLE entrega (
 	numero VARCHAR (8) NOT NULL,
 	cep CHAR (8) NOT NULL CHECK (cep REGEXP '^[0-9]{8}$'),
 	id_entregador INT NOT NULL,
-    
-    FOREIGN KEY (id_nota_fiscal)
-    REFERENCES nota_fiscal (id_nota_fiscal),
+
+	FOREIGN KEY (id_nota_fiscal)
+	REFERENCES nota_fiscal (id_nota_fiscal),
 
 	FOREIGN KEY (id_entregador)
-    REFERENCES entregador (id_entregador),
+	REFERENCES entregador (id_entregador),
 
 	FOREIGN KEY (id_estado)
-    REFERENCES estado (id_estado)
+	REFERENCES estado (id_estado)
 );
 
 CREATE TABLE motivo (
@@ -267,7 +267,7 @@ CREATE TABLE reembolso (
 		'em analise'
 	) DEFAULT 'em analise' NOT NULL,
 
-	valor_reembolso DECIMAL NOT NULL,
+	valor_reembolso DECIMAL(10, 2) NOT NULL,
 	id_nota_fiscal INT NOT NULL,
 	id_motivo INT NOT NULL,
 
@@ -280,7 +280,7 @@ CREATE TABLE reembolso (
 
 CREATE TABLE cashback (
 	id_cashback INT PRIMARY KEY AUTO_INCREMENT,
-	valor_cash DECIMAL NOT NULL,
+	valor_cash DECIMAL(10, 2) NOT NULL,
 
 	status_cash ENUM (
 		'pendente',
@@ -302,8 +302,8 @@ CREATE TABLE avaliacao (
 	id_produto INT NOT NULL,
 
 	FOREIGN KEY (id_cliente)
-    REFERENCES cliente (id_cliente),
+	REFERENCES cliente (id_cliente),
 
 	FOREIGN KEY (id_produto)
-    REFERENCES produto (id_produto)
+	REFERENCES produto (id_produto)
 );
